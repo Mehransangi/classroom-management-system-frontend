cd579cebb19789699135e6dc2e5458b8fceb6972
Mehransangi <146559254+Mehransangi@users.noreply.github.com>
2026-03-16 22:17:56 +0500

    implemented mock data provider

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+export const DEPEARTMENTS = [
+    'CS',
+    'Maths',
+    'English'
+];
+
+export const DEPARTMENT_OPTIONS = DEPEARTMENTS.map((depart) => ({
+    value: depart,
+    label: depart
+}))
